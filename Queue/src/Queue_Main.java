d33f29316eecfb3b947b5effee576ba43914db1d
Sanket <sanketstn@gmail.com>
2015-12-22 12:36:35 +0530

    Sixth commit

--- Queue/src/Queue_Main.java
+++ Queue/src/Queue_Main.java
@@ -6,15 +6,22 @@ public class Queue_Main {
 			Queue_Array arr=new Queue_Array(5);
 		
 			arr.enqueue(2);
+			
 			arr.enqueue(15);
+			
 			arr.enqueue(30);
-			
 			arr.display();
-			
 			arr.dequeue();
 			
+			arr.enqueue(40);
+			arr.enqueue(50);
+			arr.enqueue(60);
+			
 			arr.display();
-					
+			
+			
+			
+			arr.display();
 	}
 
 }
