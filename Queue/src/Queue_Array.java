f2f7a972d5ebc917460f2a230fff36650af663f5
Sanket <sanketstn@gmail.com>
2015-12-22 10:40:11 +0530

    First commit

---
+++ Queue/src/Queue_Array.java
@@ -0,0 +1,21 @@
+
+public class Queue_Array {
+
+	int capacity=5;
+	
+		public void enqueue(int element)
+		{
+			
+		}
+		
+		public Queue_Array(int capacity) {
+			super();
+			this.capacity = capacity;
+		}
+
+		public void dequeue()
+		{
+			
+		}
+	
+}
---
+++ Queue/src/Queue_Main.java
@@ -0,0 +1,10 @@
+
+public class Queue_Main {
+
+	public static void main(String[] args) {
+		
+			Queue_Array arr=new Queue_Array(5);
+		
+	}
+
+}
