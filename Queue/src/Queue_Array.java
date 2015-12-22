59db888f100ec0b46a9462d5181149e307dfeeb1
Sanket <sanketstn@gmail.com>
2015-12-22 11:17:59 +0530

    Second commit

--- Queue/src/Queue_Array.java
+++ Queue/src/Queue_Array.java
@@ -1,19 +1,49 @@
 
 public class Queue_Array {
 
-	int capacity=5;
+	int capacity,front,rear;
+	int array[];
 	
-		public void enqueue(int element)
+	
+		public boolean enqueue(int element)
 		{
-			
+			 if (rear <= capacity - 1) {  
+				 array[rear]=element;
+				   return true;
+			 }
+			 return false;
 		}
 		
 		public Queue_Array(int capacity) {
 			super();
+			this.front=0;
+			this.rear=0;
 			this.capacity = capacity;
+			this.array= new int [capacity]; 
 		}
 
 		public void dequeue()
+		{
+			
+		}
+		
+		public boolean isFull(){
+			if(rear==(capacity-1))
+			{
+				return true;
+			}
+			return false;
+		}
+
+		public int getCapacity() {
+			return capacity;
+		}
+
+		public void setCapacity(int capacity) {
+			this.capacity = capacity;
+		}
+		
+		public void display()
 		{
 			
 		}
--- Queue/src/Queue_Main.java
+++ Queue/src/Queue_Main.java
@@ -4,7 +4,9 @@ public class Queue_Main {
 	public static void main(String[] args) {
 		
 			Queue_Array arr=new Queue_Array(5);
-		
+			if(arr.enqueue(2))
+				System.out.println("sdsf");
+					
 	}
 
 }
