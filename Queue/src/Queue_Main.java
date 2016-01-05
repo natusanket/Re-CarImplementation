783d73736b3f607632ec087d95456468c2994f23
Sanket <sanketstn@gmail.com>
2016-01-05 12:12:08 +0530

    Latest Commit

--- Queue/src/Queue_Main.java
+++ Queue/src/Queue_Main.java
@@ -8,12 +8,12 @@ public class Queue_Main {
 			Queue_methods m=new Queue_Array(10);
 			Queue_methods aList=new Queue_AList(10);
 			
-			/*m.enqueue(10);
+			m.enqueue(10);
 			m.enqueue(15);
 			m.enqueue(20);
 			m.display();
 			m.dequeue();
-			m.display();*/
+			m.display();
 			
 			aList.enqueue(15);
 			aList.enqueue(20);
@@ -22,24 +22,7 @@ public class Queue_Main {
 			aList.display();
 			
 			
-			/*Queue_Array arr=new Queue_Array(5);
-		arr.enqueue(7);
-			arr.enqueue(2);
 			
-			arr.enqueue(15);
-			
-			arr.enqueue(30);
-			arr.display();
-			arr.dequeue();
-			
-			arr.enqueue(40);
-			arr.enqueue(50);
-			arr.enqueue(60);
-			
-			arr.display();
-
-			
-			arr.display();*/
 	}
 
 }
